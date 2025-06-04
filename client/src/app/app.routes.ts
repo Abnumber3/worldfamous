e5d30a5eb46c66b85bcdb24445a9dcf8d765692b
Ashton Hatchett <therealashtonhatchett@gmail.com>
2025-06-03 23:55:53 -0400

    Html update

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -9,9 +9,9 @@ import { NotFoundComponent } from './core/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'test-error', component: TestErrorComponent},
-    {path: 'server-error', component: ServerErrorComponent}, 
-    {path: 'not-found', component:NotFoundComponent}, 
+    {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Error'}}, // Route for testing errors
+    {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
+    {path: 'not-found', component:NotFoundComponent, data: {breadcrumb: 'Server Error'}},
     {
       path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)
     
