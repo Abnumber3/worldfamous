760fc52f89cd964ad12cf8b9841a8ed65be5bbe3
Ashton Hatchett <therealashtonhatchett@gmail.com>
2025-07-03 22:15:36 -0400

    updated basket repository error

--- Core/Interfaces/IBasketRepository.cs
+++ Core/Interfaces/IBasketRepository.cs
@@ -2,15 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
 
 namespace Core.Interfaces
 {
     public interface IBasketRepository1
-    {
-        Task<CustomerBasket> GetBasketAsync(string basketId);
-    }
-
-    public interface IBasketRepository
     {
         Task<CustomerBasket> GetBasketAsync(string basketId);
     }
