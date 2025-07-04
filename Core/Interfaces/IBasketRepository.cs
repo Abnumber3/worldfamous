d8d99262027f708368c76d993a7c5db5e7fe749c
Ashton Hatchett <therealashtonhatchett@gmail.com>
2025-07-04 16:41:33 -0400

    upadated basket repository

--- Core/Interfaces/IBasketRepository.cs
+++ Core/Interfaces/IBasketRepository.cs
@@ -6,8 +6,10 @@ using Core.Entities;
 
 namespace Core.Interfaces
 {
-    public interface IBasketRepository1
+    public interface IBasketRepository
     {
         Task<CustomerBasket> GetBasketAsync(string basketId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string basketId);
     }
 }
