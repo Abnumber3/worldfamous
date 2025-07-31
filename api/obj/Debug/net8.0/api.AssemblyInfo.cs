1fccef3985d12a7e6fc800ef30379cd82bbca11f
Ashton Hatchett <therealashtonhatchett@gmail.com>
2025-07-30 23:53:41 -0400

    nuget package add

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9f0003917791a4d0224098f961fdecc60c3dc7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0738c6031bd904b28f8c0fdaa4959d28351e656")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
