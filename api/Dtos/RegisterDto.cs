c102f0ef648636be284a2ba2c9d3df5802c2a7be
Ashton Hatchett <therealashtonhatchett@gmail.com>
2025-09-19 23:45:09 -0400

    update app user

--- api/Dtos/RegisterDto.cs
+++ api/Dtos/RegisterDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,12 @@ namespace api.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
         public string Email { get; set; }
-
+        [Required]
+        [RegularExpression()]
         public string Password { get; set; }
-
     }
 }
--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef7dd8896f35a72686dbfdbeaeedb6adbd8b65c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e64c4be5ec734d98fd7baba044552926e01daa")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
