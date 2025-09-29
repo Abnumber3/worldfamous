7395a0f129caee9555b2ce78e88130f6129cd5da
Ashton Hatchett <therealashtonhatchett@gmail.com>
2025-09-28 23:52:02 -0400

    Form validator for email

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+116b0530be3c7ac64b7a698a05ced715731489e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa9bdc5880348edc4d1f7f108154746d36c037b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
