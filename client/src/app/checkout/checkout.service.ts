74d7299e7cd30f14c10d7430f5c8a575114a55db
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2025-11-18 23:53:11 -0500

    updated checkout service

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,17 @@ baseUrl = 'https://localhost:5187/api/'
 
 
   constructor(private https: HttpClient) { }
-}
+
+
+
+  //  getDeliveryMethods() {
+  //   return this.http.get<DeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods').pipe(
+  //     map(dm => {
+  //       return dm.sort((a, b) => b.price - a.price)
+  //     })
+  //   )
+  // }
+
+
+
+  };
