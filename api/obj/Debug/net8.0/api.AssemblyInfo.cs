88ffe25b254b5f6b63259c2ba22b9b267a2a75c8
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2025-12-08 22:51:43 -0500

    checkout component with steppers updated

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be10ea465e12504ed23794fb810452105d18037")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0dc30e470db8406ef1dcd8efcccc1a59742c4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
