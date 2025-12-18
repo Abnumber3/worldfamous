ad7ecab179685e41ac90c6b78667407b3e3351bd
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2025-12-17 23:30:30 -0500

    updated checkout address component logic

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { AccountService } from '../../account/account.service';
 
 @Component({
   selector: 'app-checkout-address',
@@ -12,10 +13,20 @@ export class CheckoutAddressComponent {
 
   // Parent passes in the main checkout form
   @Input() checkoutForm!: FormGroup;
-  
+
+
+constructor(private accountService: AccountService) {}
+
 
   // Optional convenience getter if you ever need addressForm in TS
   get addressForm(): FormGroup {
     return this.checkoutForm.get('addressForm') as FormGroup;
+  }
+
+
+  saveUserAddress(){
+    this.accountService.updateUserAddress(this.checkoutForm.get('addressForm')?.value).subscribe({
+
+    })
   }
 }
