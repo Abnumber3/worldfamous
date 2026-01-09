5f15ed4e22929892aff93994930041b404cec1c8
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-01-08 22:24:39 -0500

    added payment to checkout review and toastr

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eedcdc0c3dda6a9e464861347c8a1bbc39f0f22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1548d2dcee5c55525ecad1ac0e6112cf2cd99f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { BasketService } from '../../basket/basket.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-checkout-review',
@@ -10,6 +12,19 @@ import { FormGroup } from '@angular/forms';
 export class CheckoutReviewComponent {
 
   @Input() checkoutForm!: FormGroup;
+
+
+  constructor(
+    private basketService: BasketService,
+    private toastr: ToastrService
+  ){}
+
+  createPaymentIntent(){
+    this.basketService.createPaymentIntent().subscribe({
+      next: () => this.toastr.success('Payment intent created'),
+      error: error => this.toastr.error(error.message)
+    })
+  }
 
 
 }
