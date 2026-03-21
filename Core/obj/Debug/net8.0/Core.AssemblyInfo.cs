2f5ef0154f458d7a0ff8b7cfd21e672d8c74da04
Abnumber3 <114044297+Abnumber3@users.noreply.github.com>
2026-03-20 21:50:36 -0400

    upadate ui

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c378ec0c07d023b2a30e0892d10f9aaeb5df64bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff78a6ade2238a49b391df0aa19108ee3b98d274")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
